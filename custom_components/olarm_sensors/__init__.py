bb2b68a9f588839ea47eafc3041bf4d5753d64b3
Tim Van <timvdwest@gmail.com>
2023-04-07 15:37:20 +0200

    Update exceptions

--- custom_components/olarm_sensors/__init__.py
+++ custom_components/olarm_sensors/__init__.py
@@ -53,7 +53,7 @@ async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry):
                 config_entry, data=data, options=options
             )
 
-    except DictionaryKeyError:
+    except KeyError:
         data = {**config_entry.data}
         options = {**config_entry.options}
         if options[CONF_ALARM_CODE] is not None and data[CONF_ALARM_CODE] is not None:
@@ -129,7 +129,7 @@ async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry):
                 if name == "":
                     name = f"area_{area+1}"
 
-            except ListIndexError:
+            except IndexError:
                 name = f"area_{area+1}"
 
             name = "".join(name.lower().split(" "))
@@ -241,7 +241,7 @@ async def update_listener(hass, entry):
 
             hass.config_entries.async_update_entry(entry, data=data, options=options)
 
-    except DictionaryKeyError:
+    except KeyError:
         data = {**entry.data}
         options = {**entry.options}
         options[CONF_ALARM_CODE] = data[CONF_ALARM_CODE]
